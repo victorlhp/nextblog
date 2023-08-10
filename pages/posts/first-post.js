de526f4821d858dde986138ba469adb2a9e8e34e
Victor Lima <victor270196@gmail.com>
2023-08-10 14:39:42 -0300

    adição de import link

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function FirstPost() {
-    return <h1>Te amo Lara</h1>;
+    return (
+    <>
+    <h1>Te amo Lara</h1>
+    <h2>
+        <Link href="/">Back to Home</Link>
+    </h2>
+   </>
+);  
 }
