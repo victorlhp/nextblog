8ac736f7d229a47e62f4d6300e3725b2b0d2e38b
Victor Lima <victor270196@gmail.com>
2023-08-17 14:31:37 -0300

    adiciona componente layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-    <>
+   
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -19,6 +21,7 @@ export default function FirstPost() {
     <h2>
         <Link href="/">Back to Home</Link>
     </h2>
-   </>
+    </Layout>
+   
 );  
 }
